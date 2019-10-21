628420c755282d8d58c69399f7f16983e17d579c
Deepa <deepa_g@techjoomla.com>
2019-10-21 17:32:48 +0530

    Task #152229 chore: Set default database engine to InnoDB and CHARACTER to utf8mb4 and COLLATE to utf8mb4_unicode_ci

--- src/admin/sql/updates/mysql/1.0.2.sql
+++ src/admin/sql/updates/mysql/1.0.2.sql
@@ -6,5 +6,15 @@ ALTER TABLE `#__tj_activities` ENGINE = InnoDB;
 --
 -- Change default table CHARACTER to utf8mb4 and COLLATE to utf8mb4_unicode_ci;
 --
-ALTER TABLE `#__tj_activities` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+ALTER TABLE `#__tj_activities` CHANGE `actor` `actor` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `actor_id` `actor_id` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `object` `object` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `object_id` `object_id` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `target` `target` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `target_id` `target_id` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `type` `type` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `client` `client` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `template` `template` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `default_theme` `default_theme` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `formatted_text` `formatted_text` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `#__tj_activities` CHANGE `location` `location` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
