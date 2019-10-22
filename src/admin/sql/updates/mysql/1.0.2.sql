a842cde1ea9963090fc49c6a8d047c5513266545
“ankush_m” <ankush_m@techjoomla.com>
2019-10-22 14:30:28 +0530

    Task #42 feat: Update default DB engine and charser of the tables

--- src/admin/sql/updates/mysql/1.0.2.sql
+++ src/admin/sql/updates/mysql/1.0.2.sql
@@ -1,5 +1,3 @@
 ALTER TABLE `#__tj_activities` MODIFY `template` text;
 ALTER TABLE `#__tj_activities` MODIFY `formatted_text` text;
-
 ALTER TABLE `#__tj_activities` ENGINE = InnoDB;
-ALTER TABLE `#__tj_activities` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
